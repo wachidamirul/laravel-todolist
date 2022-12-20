83e54cd08863ea2414f1620c5ab6248656a513f0
Wachid Amirul M <me@wachidamirul.com>
2022-12-20 14:32:24 +0700

    User Controller

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
+});
+
+Route::view('/template', 'template');
+
+Route::controller(UserController::class)->group(function () {
+	Route::get('/login', 'login');
+	Route::post('/login', 'doLogin');
+	Route::post('/logout', 'doLogout');
 });
