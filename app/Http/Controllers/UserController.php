0c72567a625294970fb84dc493784dd5ee522599
Wachid Amirul M <me@wachidamirul.com>
2022-12-20 15:13:30 +0700

    Login Action

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\UserService;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
+
+	public function __construct(UserService $userService)
+	{
+		$this->userService = $userService;
+	}
+
 	public function login(): Response
 	{
 		return response()->view('user.login', [
@@ -14,8 +22,28 @@ class UserController extends Controller
 		]);
 	}
 
-	public function doLogin()
+	public function doLogin(Request $request): Response|RedirectResponse
 	{
+		$user = $request->input('user');
+		$password = $request->input('password');
+
+		// validate input
+		if (empty($user) || empty($password)) {
+			return response()->view("user.login", [
+				"title" => "Login",
+				"error" => "User or password is required"
+			]);
+		}
+
+		if ($this->userService->login($user, $password)) {
+			$request->session()->put("user", $user);
+			return redirect("/");
+		}
+
+		return response()->view("user.login", [
+			"title" => "Login",
+			"error" => "User or password is wrong"
+		]);
 	}
 
 	public function doLogout()
--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -12,4 +12,27 @@ class UserControllerTest extends TestCase
 	{
 		$this->get('/login')->assertSeeText("Login");
 	}
+
+	public function testLoginSuccess()
+	{
+		$this->post('/login', [
+			"user" => "joko",
+			"password" => "05120512"
+		])->assertRedirect("/")
+			->assertSessionHas("user", "joko");
+	}
+
+	public function testLoginValidationError()
+	{
+		$this->post("/login", [])
+			->assertSeeText("User or password is required");
+	}
+
+	public function testLoginFailed()
+	{
+		$this->post('/login', [
+			'user' => "wrong",
+			"password" => "wrong"
+		])->assertSeeText("User or password is wrong");
+	}
 }
